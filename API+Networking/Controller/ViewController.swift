84d326912992e4fc89e32f650ae2ce7b2095d42f
AlekseyK <aleksey.kabishau@gmail.com>
2019-05-03 22:30:07 -0400

    logic of getting dog image from url is moved to model class; added the method that updates the UI

--- API+Networking/Controller/ViewController.swift
+++ API+Networking/Controller/ViewController.swift
@@ -3,48 +3,41 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let randomImageEndpoint = DogAPI.EndPoint.randomImageFromAllDogsCollection.url
         
-        /*
         let task = URLSession.shared.dataTask(with: randomImageEndpoint) { (data, response, error) in
             guard let data = data else { return }
-            print(data)
-            do {
-                let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                print(json)
-                let stringUrl = json["message"] as! String
-                print(stringUrl)
-            } catch {
-                print(error)
-            }
-        }
-        task.resume()
-        */
-        
-        let task = URLSession.shared.dataTask(with: randomImageEndpoint) { (data, response, error) in
-            guard let data = data else { return }
-
+            
             let decoder = JSONDecoder()
             //TODO: - What is the right way to do this?
             let dogImage = try! decoder.decode(DogImage.self, from: data)
             
             guard let imageUrl = URL(string: dogImage.message) else { return }
-            let task = URLSession.shared.dataTask(with: imageUrl, completionHandler: { (data, response, error) in
-                guard let data = data else { return }
-                let downloadedImage = UIImage(data: data)
-                
+            
+            /*
+            DogAPI.requestImageFile(url: imageUrl, completionHandler: { (image, error) in
+                //TODO: - How to handle Error properly?
                 DispatchQueue.main.async {
-                    self.imageView.image = downloadedImage
+                    self.imageView.image = image
                 }
             })
-            task.resume()
+            */
+            
+            // not sure how this works... it replaces the code above
+            DogAPI.requestImageFile(url: imageUrl, completionHandler: self.handleImageFileRespose(image:error:))
         }
         task.resume()
     }
-
+    
+    func handleImageFileRespose(image: UIImage?, error: Error?) {
+        DispatchQueue.main.async {
+            self.imageView.image = image
+        }
+    }
+    
 }
 
--- API+Networking/Model/DogAPI.swift
+++ API+Networking/Model/DogAPI.swift
@@ -1,4 +1,5 @@
 import Foundation
+import UIKit
 
 class DogAPI {
     
@@ -8,6 +9,20 @@ class DogAPI {
         var url: URL {
             return URL(string: self.rawValue)!
         }
+    }
+    
+    // this is helper fuction
+    // completion handler will be executed later
+    class func requestImageFile(url: URL, completionHandler: @escaping (UIImage?, Error?) -> Void) {
         
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                completionHandler(nil, error)
+                return
+            }
+            let downloadedImage = UIImage(data: data)
+            completionHandler(downloadedImage, nil)
+        }
+        task.resume()
     }
 }
